043cfa8138e825a226806f62a46efc088b906cae
MeGallin <me@garyallin.uk>
2024-12-16 18:25:43 +0000

    basci routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
-  return <>Chat app</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
