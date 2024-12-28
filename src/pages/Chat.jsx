9e42f8cf245620477f829fbe8b1359a80d56cdbc
MeGallin <me@garyallin.uk>
2024-12-28 12:19:56 +0000

    minor tweaks

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,10 +1,11 @@
-
 import { useContext } from 'react';
 import { ChatContext } from '../context/ChatContext';
-import { Container, Stack } from 'react-bootstrap';
-import UserChat from '../components/chat/UserChat';
 import { AuthContext } from '../context/AuthContext';
+
+import { Container, Row, Col, Stack } from 'react-bootstrap';
+
 import PotentialChats from '../components/chat/PotentialChats';
+import UserChat from '../components/chat/UserChat';
 import ChatBox from '../components/chat/ChatBox';
 
 const Chat = () => {
@@ -13,30 +14,41 @@ const Chat = () => {
     useContext(ChatContext);
 
   return (
-    <Container>
+    <Container fluid className="py-3">
       <PotentialChats />
+
       {userChats?.length < 1 ? null : (
-        <Stack direction="horizontal" gap={4} className="align-items-start">
-          <Stack
-            className="messages-box flex-grow-0 pe-3"
-            gap={3}
-            style={{ height: 'calc(100vh - 3.75rem - 3.75rem)' }}
+        <Row className="vh-100">
+          <Col
+            xs={12}
+            md={4}
+            className="border-end mb-3 mb-md-0 d-flex flex-column"
           >
-            {isUserChatsLoading && <p>Loading...</p>}
-            {userChats?.map((chat) => {
-              return (
-                <div
-                  key={chat._id}
-                  onClick={() => updateCurrentChat(chat)}
-                  style={{ cursor: 'pointer' }}
-                >
-                  <UserChat chat={chat} user={user} />
-                </div>
-              );
-            })}
-          </Stack>
-          <ChatBox />
-        </Stack>
+            {/* This inner div grows to fill the available vertical space */}
+            <div className="flex-grow-1 overflow-auto">
+              {isUserChatsLoading && <p>Loading...</p>}
+
+              {!isUserChatsLoading &&
+                userChats?.map((chat) => (
+                  <div
+                    key={chat._id}
+                    onClick={() => updateCurrentChat(chat)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <UserChat chat={chat} user={user} />
+                  </div>
+                ))}
+            </div>
+          </Col>
+
+          {/* Example second column for ChatBox, also grows to fill height */}
+          <Col xs={12} md={8} className="d-flex flex-column">
+            <div className="flex-grow-1 overflow-auto">
+              {/* Your ChatBox or other content */}
+              <ChatBox />
+            </div>
+          </Col>
+        </Row>
       )}
     </Container>
   );
