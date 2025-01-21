c2030dd7b11bb18e4008584fdefb35982af9c0bf
MeGallin <me@garyallin.uk>
2025-01-21 13:57:50 +0000

    update context

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -280,7 +280,8 @@ export const ChatContextProvider = ({ children, user }) => {
     }
 
     // Update the local messages state for the sender
-    setMessages((prev) => prev.filter((message) => message._id !== messageId));
+    // setMessages((prev) => prev.filter((message) => message._id !== messageId));
+    fetchMessages(); // Re-fetch messages for the current chat
   };
 
   useEffect(() => {
